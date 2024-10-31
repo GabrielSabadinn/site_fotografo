3d5505d8260badf0d5b7558934c55f4a569d5f47
Gabriel Sabadin <gbsabadin@icloud.com>
2024-10-31 10:02:21 -0300

    in progress

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/style.css'; 
-
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-createApp(App)
-  .use(router)
-  .mount('#app');
+// Crie a instância do Vue e use o Toast
+const app = createApp(App);
+
+app.use(router);
+app.use(Toast); // Adicione esta linha para usar o toast
+
+app.mount('#app');
